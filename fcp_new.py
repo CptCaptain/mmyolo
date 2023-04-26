52f1ff739befb060120320175801ecc0eae6f064
Cpt.Captain <nilskoch@posteo.de>
2023-04-26 17:06:20 +0200

    add updated copy-paste mechanism (still doesnt work)

--- fcp_new.py
+++ fcp_new.py
@@ -1,27 +1,31 @@
+# Copyright (c) OpenMMLab. All rights reserved.
 import copy
 import random
 import numpy as np
 import collections
+from typing import Sequence, Union
 
-# from mmdet.registry import TRANSFORMS, DATASETS
-# from mmdet.datasets import CocoDataset
 from mmyolo.datasets import YOLOv5CocoDataset
+from mmengine.dataset import BaseDataset, force_full_init
+from mmcv.transforms import BaseTransform
 from mmyolo.registry import TRANSFORMS, DATASETS
-from mmdet.structures.mask import BitmapMasks
+from mmcv.transforms.utils import avoid_cache_randomness, cache_randomness
+
+from mmdet.structures.bbox import HorizontalBoxes, autocast_box_type
+from mmdet.structures.mask import BitmapMasks, PolygonMasks
+
 import mmengine
 
 
 @DATASETS.register_module()
 class MultiImageMixDataset:
     """A wrapper of multiple images mixed dataset.
-
     Suitable for training on multiple images mixed data augmentation like
     mosaic and mixup. For the augmentation pipeline of mixed image data,
     the `get_indexes` method needs to be provided to obtain the image
     indexes, and you can set `skip_flags` to change the pipeline running
     process. At the same time, we provide the `dynamic_scale` parameter
     to dynamically change the output image size.
-
     Args:
         dataset (:obj:`CustomDataset`): The dataset to be mixed.
         pipeline (Sequence[dict]): Sequence of transform object or
@@ -37,19 +41,11 @@ class MultiImageMixDataset:
     """
 
     def __init__(self,
-                 dataset,
-                 pipeline,
-                 dynamic_scale=None,
-                 skip_type_keys=None,
-                 max_refetch=15,
-                 # test_mode get's set vor val and test.
-                 # We don't use it, but need to expect it being there
-                 test_mode=False,
-                 ):
-        if dynamic_scale is not None:
-            raise RuntimeError(
-                'dynamic_scale is deprecated. Please use Resize pipeline '
-                'to achieve similar functions')
+                 dataset: Union[BaseDataset, dict],
+                 pipeline: Sequence[str],
+                 skip_type_keys: Union[Sequence[str], None] = None,
+                 max_refetch: int = 15,
+                 lazy_init: bool = False) -> None:
         assert isinstance(pipeline, collections.abc.Sequence)
         if skip_type_keys is not None:
             assert all([
@@ -68,16 +64,57 @@ class MultiImageMixDataset:
             else:
                 raise TypeError('pipeline must be a dict')
 
-        dataset = DATASETS.build(dataset)
-        self.dataset = dataset
-        self.CLASSES = dataset.metainfo['classes']
-        self.PALETTE = getattr(dataset, 'PALETTE', None)
-        if hasattr(self.dataset, 'flag'):
-            self.flag = dataset.flag
-        self.num_samples = len(dataset)
-        self.max_refetch = max_refetch
-        self.test_mode = test_mode
+        self.dataset: BaseDataset
+        if isinstance(dataset, dict):
+            self.dataset = DATASETS.build(dataset)
+        elif isinstance(dataset, BaseDataset):
+            self.dataset = dataset
+        else:
+            raise TypeError(
+                'elements in datasets sequence should be config or '
+                f'`BaseDataset` instance, but got {type(dataset)}')
 
+        self._metainfo = self.dataset.metainfo
+        if hasattr(self.dataset, 'flag'):
+            self.flag = self.dataset.flag
+        self.num_samples = len(self.dataset)
+        self.max_refetch = max_refetch
+
+        self._fully_initialized = False
+        if not lazy_init:
+            self.full_init()
+
+    @property
+    def metainfo(self) -> dict:
+        """Get the meta information of the multi-image-mixed dataset.
+        Returns:
+            dict: The meta information of multi-image-mixed dataset.
+        """
+        if not hasattr(self, 'metainfo'):
+            self.metainfo = copy.deepcopy(self._metainfo)
+        return self.metainfo
+
+
+    def full_init(self):
+        """Loop to ``full_init`` each dataset."""
+        if self._fully_initialized:
+            return
+
+        self.dataset.full_init()
+        self._ori_len = len(self.dataset)
+        self._fully_initialized = True
+
+    @force_full_init
+    def get_data_info(self, idx: int) -> dict:
+        """Get annotation by index.
+        Args:
+            idx (int): Global index of ``ConcatDataset``.
+        Returns:
+            dict: The idx-th annotation of the datasets.
+        """
+        return self.dataset.get_data_info(idx)
+
+    @force_full_init
     def __len__(self):
         return self.num_samples
 
@@ -128,7 +165,6 @@ class MultiImageMixDataset:
 
     def update_skip_type_keys(self, skip_type_keys):
         """Update skip_type_keys. It is called by an external hook.
-
         Args:
             skip_type_keys (list[str], optional): Sequence of type
                 string to be skip pipeline.
@@ -138,9 +174,12 @@ class MultiImageMixDataset:
         ])
         self._skip_type_keys = skip_type_keys
 
+
 @TRANSFORMS.register_module()
-class FineTuneCopyPaste:
-    """
+class FineTuneCopyPaste(BaseTransform):
+    """This is a variant for Simple Copy-Paste, that takes a secondary dataset of 
+    images of new classes, and copies the instances from this set into the main 
+    dataset.
     Simple Copy-Paste is a Strong Data Augmentation Method for Instance
     Segmentation The simple copy-paste transform steps are as follows:
     1. The destination image is already resized with aspect ratio kept,
@@ -157,34 +196,35 @@ class FineTuneCopyPaste:
        filter some objects which are totally occluded, and adjust bboxes
        which are partly occluded.
     7. Append selected source bboxes, masks, and labels.
+    Required Keys:
+    - img
+    - gt_bboxes (BaseBoxes[torch.float32]) (optional)
+    - gt_bboxes_labels (np.int64) (optional)
+    - gt_ignore_flags (bool) (optional)
+    - gt_masks (BitmapMasks) (optional)
+    Modified Keys:
+    - img
+    - gt_bboxes (optional)
+    - gt_bboxes_labels (optional)
+    - gt_ignore_flags (optional)
+    - gt_masks (optional)
     Args:
-        supl_dataset_cfg (dict): Dataset config to be used for the supplemental
-            Dataset. Must include Pipeline.
-            Default: None.      (set this yourself)
-        copy_paste_chance (float): Apply this only to a subset of images to avoid
-            severely unbalancing the dataset.
-            Default: 0.3.
         max_num_pasted (int): The maximum number of pasted objects.
-            Default: 100.
+            Defaults to 100.
         bbox_occluded_thr (int): The threshold of occluded bbox.
-            Default: 10.
+            Defaults to 10.
         mask_occluded_thr (int): The threshold of occluded mask.
-            Default: 300.
-        selected (bool): Whether select objects or not. If select is False,
-            all objects of the source image will be pasted to the
-            destination image.
-            Default: True.
+            Defaults to 300.
     """
 
     def __init__(
-        self,
-        supl_dataset_cfg=None,
-        copy_paste_chance=0.3,
-        max_num_pasted=100,
-        bbox_occluded_thr=10,
-        mask_occluded_thr=300,
-        selected=True,
-    ):
+            self,
+            supl_dataset_cfg=None,
+            copy_paste_chance=0.3,
+            max_num_pasted: int = 100,
+            bbox_occluded_thr: int = 10,
+            mask_occluded_thr: int = 300,
+        ) -> None:
         if supl_dataset_cfg is None:
             raise Exception('Supplementary Dataset Path is required for fine-tune copy paste augmentation')
 
@@ -193,11 +233,10 @@ class FineTuneCopyPaste:
         self.max_num_pasted = max_num_pasted
         self.bbox_occluded_thr = bbox_occluded_thr
         self.mask_occluded_thr = mask_occluded_thr
-        self.selected = selected
 
     def _get_supl_dataset(self, dataset_cfg):
         cfg_dict = dict(
-            type='MultiImageMixDataset',
+            type='mmdet.MultiImageMixDataset',
             dataset=dict(
                 type='YOLOv5CocoDataset',
                 ann_file=dataset_cfg['ann_file'],
@@ -213,25 +252,25 @@ class FineTuneCopyPaste:
         cfg = mmengine.Config(cfg_dict)
         self.supl_dataset = DATASETS.build(cfg_dict)
 
-    def get_indexes(self, dataset):
+    @cache_randomness
+    def get_indexes(self, dataset: BaseDataset) -> int:
         """Call function to collect indexes.s.
         Args:
             dataset (:obj:`MultiImageMixDataset`): The dataset.
         Returns:
             list: Indexes.
         """
-        # return random.randint(0, len(dataset))
-        # FIXME len -1 due to IndexErrors in the iterator. But Why?
-        # https://wandb.ai/nkoch-aitastic/van-detection/runs/3lvi3hql/logs?
-        return random.randint(0, len(dataset)-1)
+        return random.randint(0, len(dataset))
 
-    def __call__(self, results):
-        """Call function to make a copy-paste of image.
+    @autocast_box_type()
+    def transform(self, results: dict) -> dict:
+        """Transform function to make a copy-paste of image.
         Args:
             results (dict): Result dict.
         Returns:
             dict: Result dict with copy-paste transformed.
         """
+
         # only apply to limited amount of images
         if random.uniform(0, 1) > self.copy_paste_chance:
             return results
@@ -240,26 +279,33 @@ class FineTuneCopyPaste:
         supl_item = self.supl_dataset.__getitem__(supl_idx)
         return self._copy_paste(results, supl_item)
 
-    def _select_object(self, results):
+    @cache_randomness
+    def _get_selected_inds(self, num_bboxes: int) -> np.ndarray:
+        max_num_pasted = min(num_bboxes + 1, self.max_num_pasted)
+        num_pasted = np.random.randint(0, max_num_pasted)
+        return np.random.choice(num_bboxes, size=num_pasted, replace=False)
+
+    def _select_object(self, results: dict) -> dict:
         """Select some objects from the source results."""
         bboxes = results['gt_bboxes']
-        labels = results['gt_labels']
+        labels = results['gt_bboxes_labels']
         masks = results['gt_masks']
-        max_num_pasted = min(bboxes.shape[0] + 1, self.max_num_pasted)
-        num_pasted = np.random.randint(0, max_num_pasted)
-        selected_inds = np.random.choice(
-            bboxes.shape[0], size=num_pasted, replace=False)
+        ignore_flags = results['gt_ignore_flags']
+
+        selected_inds = self._get_selected_inds(bboxes.shape[0])
 
         selected_bboxes = bboxes[selected_inds]
         selected_labels = labels[selected_inds]
         selected_masks = masks[selected_inds]
+        selected_ignore_flags = ignore_flags[selected_inds]
 
         results['gt_bboxes'] = selected_bboxes
-        results['gt_labels'] = selected_labels
+        results['gt_bboxes_labels'] = selected_labels
         results['gt_masks'] = selected_masks
+        results['gt_ignore_flags'] = selected_ignore_flags
         return results
 
-    def _copy_paste(self, dst_results, src_results):
+    def _copy_paste(self, dst_results: dict, src_results: dict) -> dict:
         """CopyPaste transform function.
         Args:
             dst_results (dict): Result dict of the destination image.
@@ -271,57 +317,71 @@ class FineTuneCopyPaste:
         dst_bboxes = dst_results['gt_bboxes']
         dst_labels = dst_results['gt_bboxes_labels']
         dst_masks = dst_results['gt_masks']
+        dst_ignore_flags = dst_results['gt_ignore_flags']
 
         src_img = src_results['img']
         src_bboxes = src_results['gt_bboxes']
         src_labels = src_results['gt_bboxes_labels']
         src_masks = src_results['gt_masks']
+        src_ignore_flags = src_results['gt_ignore_flags']
 
         if len(src_bboxes) == 0:
             return dst_results
 
         # update masks and generate bboxes from updated masks
         composed_mask = np.where(np.any(src_masks.masks, axis=0), 1, 0)
-        updated_dst_masks = self.get_updated_masks(dst_masks, composed_mask)
-        updated_dst_bboxes = updated_dst_masks.get_bboxes()
+        # TODO investigate how these bboxes have different shapes......
+        updated_dst_masks = self._get_updated_masks(dst_masks, composed_mask)
+        updated_dst_bboxes = updated_dst_masks.get_bboxes(type(dst_bboxes))
         assert len(updated_dst_bboxes) == len(updated_dst_masks)
 
         # filter totally occluded objects
-        bboxes_inds = np.all(
-            np.abs(
-                (updated_dst_bboxes - dst_bboxes)) <= self.bbox_occluded_thr,
-            axis=-1)
+        try:
+            l1_distance = (updated_dst_bboxes.tensor - dst_bboxes.tensor).abs()
+        except:
+            print(f'{updated_dst_bboxes.tensor.v=}')
+            print(f'{dst_bboxes.tensor.v=}')
+            quit()
+        bboxes_inds = (l1_distance <= self.bbox_occluded_thr).all(
+            dim=-1).numpy()
         masks_inds = updated_dst_masks.masks.sum(
             axis=(1, 2)) > self.mask_occluded_thr
         valid_inds = bboxes_inds | masks_inds
 
         # Paste source objects to destination image directly
         img = dst_img * (1 - composed_mask[..., np.newaxis]
-                         ) + src_img * composed_mask[..., np.newaxis]
-        bboxes = np.concatenate([updated_dst_bboxes[valid_inds], src_bboxes])
+        ) + src_img * composed_mask[..., np.newaxis]
+        bboxes = src_bboxes.cat([updated_dst_bboxes[valid_inds], src_bboxes])
         labels = np.concatenate([dst_labels[valid_inds], src_labels])
         masks = np.concatenate(
             [updated_dst_masks.masks[valid_inds], src_masks.masks])
+        ignore_flags = np.concatenate(
+            [dst_ignore_flags[valid_inds], src_ignore_flags])
 
         dst_results['img'] = img
         dst_results['gt_bboxes'] = bboxes
-        dst_results['gt_labels'] = labels
+        dst_results['gt_bboxes_labels'] = labels
         dst_results['gt_masks'] = BitmapMasks(masks, masks.shape[1],
                                               masks.shape[2])
+        dst_results['gt_ignore_flags'] = ignore_flags
+
+        # masks seem problematic...
+        dst_results.pop('gt_masks')
 
         return dst_results
 
-    def get_updated_masks(self, masks, composed_mask):
+    def _get_updated_masks(self, masks: BitmapMasks,
+                           composed_mask: np.ndarray) -> BitmapMasks:
+        """Update masks with composed mask."""
         assert masks.masks.shape[-2:] == composed_mask.shape[-2:], \
-            'Cannot compare two arrays of different size'
+        'Cannot compare two arrays of different size'
         masks.masks = np.where(composed_mask, 0, masks.masks)
         return masks
 
     def __repr__(self):
         repr_str = self.__class__.__name__
-        repr_str += f'max_num_pasted={self.max_num_pasted}, '
+        repr_str += f'(max_num_pasted={self.max_num_pasted}, '
         repr_str += f'bbox_occluded_thr={self.bbox_occluded_thr}, '
         repr_str += f'mask_occluded_thr={self.mask_occluded_thr}, '
-        repr_str += f'selected={self.selected}, '
         return repr_str
 
